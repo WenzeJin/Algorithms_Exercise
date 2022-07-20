8c75e84e9623b8b957eedd6d7fd9f20230638e68
WenzeJin <andyjibo@outlook.com>
2022-07-20 22:12:00 +0800

    20220720

--- Answer/src/E1_1/Q20.java
+++ Answer/src/E1_1/Q20.java
@@ -1,10 +1,18 @@
 package E1_1;
+import java.util.*;
 
 
 public class Q20 {
     public static double ln(int N)
     {
         if(N > 0){return Math.log(N) + ln(N-1);}
-        return sum;
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        System.out.printf("%f\n",ln(N));
+        in.close();
     }
 }
